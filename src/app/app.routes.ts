b3a71a4f463d5573e6902dd00bb12b86d36df0f7
Juan  Marquina <juanantoniomarquina@gmail.com>
2025-01-05 20:59:33 -0500

    feat: se agrego la estructura de paises

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 export const routes: Routes = [
 
@@ -11,6 +12,14 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'paises',
+    component: ContactPageComponent
   },
   {
     path: '**',
---
+++ src/app/paises/pages/by-pais-page/by-pais-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-pais-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './by-pais-page.component.html',
+  styleUrl: './by-pais-page.component.css'
+})
+export class ByPaisPageComponent {
+
+}
