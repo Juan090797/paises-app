0636b813ff73cb410bded02828c870d6b708f648
Juan  Marquina <juanantoniomarquina@gmail.com>
2025-01-07 22:40:28 -0500

    feat: update page pais

--- src/app/paises/pages/by-region-page/by-region-page.component.ts
+++ src/app/paises/pages/by-region-page/by-region-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { SearchBoxComponent } from '../../../shared/components/search-box/search-box.component';
+import { CommonModule } from '@angular/common';
+import { PaisTableComponent } from '../../components/pais-table/pais-table.component';
+import { Pais } from '../../interfaces/pais';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-by-region-page',
   standalone: true,
-  imports: [],
+  imports: [SearchBoxComponent, CommonModule, PaisTableComponent],
   templateUrl: './by-region-page.component.html',
   styleUrl: './by-region-page.component.css'
 })
 export class ByRegionPageComponent {
+  public paises: Pais[] = [];
+
+      constructor( private paisesService: PaisesService){}
+
+
+      busquedaByRegion( term: string):void{
+        console.log('desde busquedaByRegion');
+        this.paisesService.searchRegion(term)
+        .subscribe( paises => {
+          this.paises = paises
+        });
+
+      }
 
 }
--- src/app/paises/pages/pais-page/pais-page.component.ts
+++ src/app/paises/pages/pais-page/pais-page.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaisesService } from '../../services/paises.service';
+import { switchMap } from 'rxjs';
+import { Pais } from '../../interfaces/pais';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pais-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pais-page.component.html',
   styleUrl: './pais-page.component.css'
 })
-export class PaisPageComponent {
+export class PaisPageComponent implements OnInit{
+
+  public pais?: Pais;
+
+  constructor(
+    private activedRoute: ActivatedRoute,
+    private paisesService: PaisesService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+
+    this.activedRoute.params
+        .pipe(
+          switchMap( ({id}) => this.paisesService.searchPaisByAlphaCode(id)),
+        )
+        .subscribe(pais =>{
+
+          if(!pais){
+            return this.router.navigateByUrl('');
+          }
+          return this.pais = pais;
+        })
+
+
+    // this.activedRoute.params
+    //     .subscribe( ({id}) =>{
+    //       this.paisesService.searchPaisByAlphaCode(id)
+    //           .subscribe( pais =>
+    //               console.log({pais})
+    //           )
+    //     })
+
+  }
 
 }
